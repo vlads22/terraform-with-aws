a7f6af705c75ed92746b711cec274d7a22266b81
vlad stefan <vlad.curteanu@yahoo.com>
2023-08-01 14:44:54 +0200

    writing internet access, in progress

--- project-2/main.tf
+++ project-2/main.tf
@@ -47,4 +47,60 @@ resource "aws_subnet" "subnet-1" {
   }
 }
 
-# Use route table association to assign subnet to route table.
+# Use route table association to associate subnet with route table.
+resource "aws_route_table_association" {
+    subnet_id = aws_subnet.subnet-1.id
+    route_table_id = aws_route_table.prod-route-table.id
+}
+
+
+# create security group that allows access only to port 22,80,443
+resource "aws_security_group" "allow_web" {
+    name = "allow_web_traffic"
+    description = "allow inbound traffic from the net"
+    vpc_id = aws_vpc.prod-vpc.ic
+
+    ingress {
+        description = "HTTPS"
+        # allow tcp traffic on port 443
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        # what subnets or ip addressed we want to allow to reach this port. in this case we want this open to the web
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        # allow tcp traffic also for http - which resides on port 80
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "SSH"
+        # allow tcp traffic for ssh on port 22
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }    
+
+    egress {
+        # 0 = we are allowing all ports in the egress direction
+        from_port = 0
+        to_port = 0
+        # -1 = all protocolos
+        protocol = "-1"
+        # = any ip address from the subnet can go in the egress direction
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "allow_web"
+    }
+
+
+    }
